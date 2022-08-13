3242029a1f0cace72b864ad6b31db82bdea54183
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-08-13 19:57:23 +0100

    .

--- FROY.py
+++ FROY.py
@@ -1,3 +1,12 @@
+import requests, json
+
+response = requests.get('http://ipinfo.io/json')
+x = json.loads(response.text)['country']
+if 'NG 'in str(x):
+  print("Enter the script")
+else:
+  print(x)
+
 P = '\x1b[1;97m'
 import os,requests
 xr = requests.get("http://ip-api.com/json/").json()
@@ -10,7 +19,7 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("FROY").keyx()
+		__import__("FROY").Checkin_()
 	else:
-		__import__("FROY").keyx()
+		__import__("FROY").Checkin_()
 
