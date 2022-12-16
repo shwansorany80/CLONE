34c26941d2139132e7c349b31ee335a6f18c6ada
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-12-16 13:25:13 +0100

    .

--- CLONE.py
+++ CLONE.py
@@ -1,21 +1,30 @@
-import os
-import platform
-import webbrowser
-os.system('termux-setup-storage')
-os.system('git pull')
-P = '\x1b[1;97m'
-import os,requests
-xr = requests.get("http://ip-api.com/json/").json()
+#coding=utf-8
+import os, sys, platform
 try:
-	fc = xr["country"]
-except KeyError:
-	print('%s\nBAD INTERNET CONNECTION\n'%(P))
-	exit()
+    import requests
+except:
+    os.system('pip install requests')
+os.system('xdg-open https://chat.whatsapp.com/F9uCvPXPJml891R0KETB6y')
+import requests
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf CLONE.so CLONE32.so')
+except:
+    pass
+os.system('rm -rf CLONE.so CLONE32.so')
+os.system('git pull')
 
-if __name__ == "__main__":
-	os.system("git pull")
-	if "Nigeria" == fc:
-		__import__("CLONE").Main_()
-	else:
-		__import__("CLONE").Main_()
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('CLONE.so'):
+        os.system('curl -L https://github.com/chigoziieworldwide/exee/blob/main/CLONE.cpython-311.so?raw=true -o CLONE.so') 
+        __import__("CLONE").Main_()
+    else:
+        __import__("CLONE").Main_()
 
+elif bit == '32bit':
+    if not os.path.isfile('CLONE32.so'):
+        os.system('curl -L https://github.com/chigoziieworldwide/exee/blob/main/CLONE32.cpython-311.so?raw=true -o CLONE32.so') 
+        __import__("CLONE32").Main_()
+    else:
+        __import__("CLONE32").Main_()
